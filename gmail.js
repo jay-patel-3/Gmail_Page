0962d1b8befb51fd9140d2b68f373c2fd1a33797
jp27-eng <71619423+jp27-eng@users.noreply.github.com>
2021-09-03 18:25:35 -0400

    Update gmail.js

--- gmail.js
+++ gmail.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 //Phone number for form validation
 const phoneInputField = document.querySelector("#phone");
@@ -6,3 +7,14 @@ const phoneInput = window.intlTelInput(phoneInputField,
     utilsScript:
     "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
+=======
+/*Functions to be added*/
+
+//Phone number for form validation
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, 
+{
+    utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+>>>>>>> Stashed changes
